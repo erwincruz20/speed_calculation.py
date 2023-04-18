73058d9f98cb2c3d2b50e297a5f64be20716cc87
erwincruz20 <125974034+erwincruz20@users.noreply.github.com>
2023-04-18 16:50:22 +0300

    Create speed_calculation.py

---
+++ speed_calculation.py
@@ -0,0 +1,6 @@
+distance = float(input("Введите расстояние в километрах: "))
+time = float(input("Введите время в часах: "))
+
+speed = distance / time
+
+print("Скорость движения:", speed, "километров в час.")
